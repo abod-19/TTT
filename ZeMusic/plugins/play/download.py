34cd09fad1481bb0878809471819f683e541275b
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-01 01:13:03 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -21,7 +21,7 @@ def remove_if_exists(path):
         
 lnk = "https://t.me/" + config.CHANNEL_LINK
 Nem = config.BOT_NAME + " ابحث"
-@app.on_message(command(["song", "/song", "بحث", Nem]))
+@app.on_message(command(["song", "/song", "بحث", Nem, "تنزيل"]))
 async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث ..</b>")
