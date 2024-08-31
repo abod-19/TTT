a946b091119562b5a6a42b4f81d36d16f4c7dbcc
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-01 02:16:48 +0300

    Update Insta.py

--- ZeMusic/plugins/Managed/Insta.py
+++ ZeMusic/plugins/Managed/Insta.py
@@ -2,6 +2,7 @@ import os
 import future
 import asyncio
 import requests
+import wget
 import time
 import yt_dlp
 from urllib.parse import urlparse
@@ -9,15 +10,25 @@ from ZeMusic import app
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-@app.on_message(filters.command(["انستا"], ["."]))
-async def download_instagram(c: app, m: Message):
+
+# ------------------------------------------------------------------------------- #
+
+
+                     #   INSTAGRAM REELS LELO SAB  #
+        
+
+# ------------------------------------------------------------------------------- #
+
+
+@app.on_message(filters.command(["انستا"], ["/", "!", "."]))
+async def download_instareels(c: app, m: Message):
     try:
         reel_ = m.command[1]
     except IndexError:
-        await m.reply_text("≭︰إرسل رابط المقطع في الانستا .")
+        await m.reply_text("أعطني رابطا لتحميل المقطع...")
         return
     if not reel_.startswith("https://www.instagram.com/reel/"):
-        await m.reply_text("≭︰عذراً ~ الرابط غير صالح .")
+        await m.reply_text("من أجل الحصول على النتائج الصحيحه ، من الضروري وجود رابط صالح. يرجى تزويدي بالرابط المطلوب.")
         return
     OwO = reel_.split(".",1)
     Reel_ = ".dd".join(OwO)
@@ -33,56 +44,24 @@ async def download_instagram(c: app, m: Message):
                 await m.reply_document(Reel_)
                 return
             except Exception:
-                await m.reply_text("Error")
+                await m.reply_text("أنا غير قادر على الوصول إلى هذه النتيجه.")
 
-@app.on_message(filters.command(["توك"], ["."]))
-async def download_tiktok(c: app, m: Message):
-    try:
-        tiktok_ = m.command[1]
-    except IndexError:
-        await m.reply_text("≭︰إرسل رابط المقطع في التيك توك .")
-        return
-    if not tiktok_.startswith("https://vt.tiktok.com/"):
-        await m.reply_text("≭︰عذراً ~ الرابط غير صالح .")
-        return
-    tiktok = tiktok_.split(".",1)
-    tiktok_ = ".dd".join(tiktok)
-    try:
-        await m.reply_video(tiktok_)
-        return
-    except Exception:
-        try:
-            await m.reply_photo(tiktok_)
-            return
-        except Exception:
-            try:
-                await m.reply_document(tiktok_)
-                return
-            except Exception:
-                await m.reply_text("Error")
 
-@app.on_message(filters.command(["يوتيوب"], ["."]))
-async def download_youtube(c: app, m: Message):
-    try:
-        youtube_ = m.command[1]
-    except IndexError:
-        await m.reply_text("≭︰إرسل رابط المقطع في اليوتيوب .")
-        return
-    if not youtube_.startswith("https://youtu.be/"):
-        await m.reply_text("≭︰عذراً ~ الرابط غير صالح .")
-        return
-    youtube = youtube_.split(".",1)
-    youtube_ = ".dd".join(youtube)
-    try:
-        await m.reply_video(youtube_)
-        return
-    except Exception:
-        try:
-            await m.reply_photo(youtube_)
-            return
-        except Exception:
-            try:
-                await m.reply_document(youtube_)
-                return
-            except Exception:
-                await m.reply_text("Error")
+@app.on_message(filters.command(["تحميل استوري"], ["/", "!", "."]))
+async def instagram_reel(client, message):
+    if len(message.command) == 2:
+        url = message.command[1]
+        response = requests.post(f"https://lexica-api.vercel.app/download/instagram?url={url}")
+        data = response.json()
+
+        if data['code'] == 2:
+            media_urls = data['content']['mediaUrls']
+            if media_urls:
+                video_url = media_urls[0]['url']
+                await message.reply_video(f"{video_url}")
+            else:
+                await message.reply("لم يتم العثور على فيديو في الرد. قد يكون الحساب خاصا.")
+        else:
+            await message.reply("لم يكن الطلب ناجحا.")
+    else:
+        await message.reply("يرجى تقديم عنوان URL صالح لـ Instagram باستخدام الأمر تحميل استوري..")
