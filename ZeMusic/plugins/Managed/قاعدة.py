165d2dd6cfbe7f3c4b46f90abf96e4a507437297
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-05 22:55:51 +0300

    Update قاعدة.py

--- ZeMusic/plugins/Managed/قاعدة.py
+++ ZeMusic/plugins/Managed/قاعدة.py
@@ -6,7 +6,7 @@ import asyncio
 import time
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserAlreadyParticipant
 import asyncio
 import random
@@ -14,6 +14,6 @@ from ZeMusic.utils.database import add_served_chat
 from ZeMusic import app
 
 @app.on_message(filters.command(["ا", "هلا", "سلام", "المالك", "بخير", "وانت"],"") & filters.group)
-async def bot_check(_, client: Client, message: Message):
+async def bot_check(client: Client, message: Message):
     chat_id = message.chat.id
     await add_served_chat(chat_id)
