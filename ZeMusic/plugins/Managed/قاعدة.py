0283ce055caae79446dbfe8493a88dd5b8e27be6
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-05 22:57:50 +0300

    Update قاعدة.py

--- ZeMusic/plugins/Managed/قاعدة.py
+++ ZeMusic/plugins/Managed/قاعدة.py
@@ -14,6 +14,6 @@ from ZeMusic.utils.database import add_served_chat
 from ZeMusic import app
 
 @app.on_message(filters.command(["ا", "هلا", "سلام", "المالك", "بخير", "وانت"],"") & filters.group)
-async def bot_check(client: Client, message: Message):
+async def bot_check(_, message):
     chat_id = message.chat.id
     await add_served_chat(chat_id)
