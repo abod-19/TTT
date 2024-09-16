fd3a7b84d59afa64210333feb7ff675fd48bb8bc
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-17 02:01:18 +0300

    Update Gpt.py

--- ZeMusic/plugins/Managed/Gpt.py
+++ ZeMusic/plugins/Managed/Gpt.py
@@ -1,39 +1,25 @@
-import random
-import time
 import requests
-from ZeMusic import app
-
-from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
+from strings.filters import command
+from ZeMusic import api, app
 
-@app.on_message(filters.command(["رون"],""))
-async def chat_gpt(bot, message):
+
+@app.on_message(command(["رون"]))
+async def bard(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text(
+            "-› اكتب رون واي شي تريد تسالة راح يجاوبك ."
+        )
+        return
+
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        user_input = " ".join(message.command[1:])
+
     try:
-        start_time = time.time()
-        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-
-        if len(message.command) < 2:
-            await message.reply_text(
-                "⟡ استخدم الأمر هكذا :\n\n ⟡ لوي + سؤالك"
-            )
-        else:
-            a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
-
-            try:
-                # Check if "results" key is present in the JSON response
-                if "answer" in response.json():
-                    x = response.json()["answer"]
-                    end_time = time.time()
-                    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
-                    await message.reply_text(
-                        f" {x} ",
-                        parse_mode=ParseMode.MARKDOWN
-                    )
-                else:
-                    await message.reply_text("لم يتم العثور على النتائج في الاستجابة.")
-            except KeyError:
-                # Handle any other KeyError that might occur
-                await message.reply_text("حدث خطأ أثناء الوصول إلى الاستجابة.")
-    except Exception as e:
-        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+        Z = await api.bard(user_input)
+        result = Z["candidates"][0]["content"]["parts"][0]["text"]
+        await message.reply_text(result)
+    except requests.exceptions.RequestException as e:
+        pass
