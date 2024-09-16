173ea02c8e61261408e94a10c1abe9cfe3976df5
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-17 01:20:48 +0300

    Update انظم&غادر.py

--- ZeMusic/plugins/Managed/انظم&غادر.py
+++ ZeMusic/plugins/Managed/انظم&غادر.py
@@ -5,34 +5,26 @@ from ZeMusic import app
 from ZeMusic.misc import SUDOERS
 from ZeMusic.utils.database import get_assistant
 from pyrogram.types import Message 
+import config
+
+Nem = config.BOT_NAME + " غادر"
 
 @app.on_message(
-    command(["انضم","ادخل","طب"]))
+    command(["المساعد انضم","انضمام المساعد","مساعد انضم"]))
 async def invite_assistant(client, message):
     try:
-        # Get the music bot assistant
         userbot = await get_assistant(message.chat.id)
-
-        # Check if the bot has admin rights in the group
         try:
             await client.get_chat_member(message.chat.id, "me")
         except ChatAdminRequired:
-            return await message.reply_text(
-                "• انطيني صلاحية اضافة مستخدمين ."
-            )
-
-        # Unban the assistant if it's banned in the group
+            return await message.reply_text("• انطيني صلاحية اضافة مستخدمين .")
         try:
             await client.unban_chat_member(message.chat.id, userbot.id)
         except:
             pass
-
-        # Get the invite link for the group
+            
         invitelink = await client.export_chat_invite_link(message.chat.id)
-
-        # Invite the assistant to the group
         await userbot.join_chat(invitelink)
-
         await message.reply_text("-› تمت اضافة المساعد بنجاح .")
 
     except InviteRequestSent:
@@ -45,16 +37,22 @@ async def invite_assistant(client, message):
         await message.reply_text(f"-› حدث خطأ .: {e}")
         
 
-@app.on_message(command(["برا","اطلع","غادر"]) & SUDOERS)
+@app.on_message(command(["المساعد غادر","مساعد غادر","مساعد مغادره"]) & SUDOERS)
 async def leave_group(client, message):
     try:
-        # Get the music bot assistant
         userbot = await get_assistant(message.chat.id)
+        await userbot.leave_chat(message.chat.id)
 
-        # Prepare a message to send before leaving the group
+        await message.reply_text("-› غادر المساعد كما طلبت.")
+
+    except Exception as e:
+        await message.reply_text(f"-› حدث خطأ أثناء مغادرة المجموعة: {e}")
+
+@app.on_message(command([Nem]) & SUDOERS)
+async def leave_group(client, message):
+    try:
+        userbot = await get_assistant(message.chat.id)
         leave_message = "شكرًا لكم جميعًا، وداعاً!"
-
-        # Leave the group
         await app.send_message(message.chat.id, leave_message)
         await userbot.leave_chat(message.chat.id)
         await app.leave_chat(message.chat.id)
