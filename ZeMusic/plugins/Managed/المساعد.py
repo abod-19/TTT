2a0cb1568f12195ef15144e950e6830681315091
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-27 18:46:53 +0300

    Update المساعد.py

--- ZeMusic/plugins/Managed/المساعد.py
+++ ZeMusic/plugins/Managed/المساعد.py
@@ -8,27 +8,18 @@ from ZeMusic.utils.database import get_assistant
 @app.on_message(command(["المساعد", "الحساب المساعد"]) & ~BANNED_USERS)
 async def assistant(c: Client, m: Message):
     userbot = await get_assistant(m.chat.id)
-    BOT_USERNAME = app.username
     usern = userbot.username
     aname = userbot.name
     idd = userbot.id
 
     info = await app.get_chat(idd)
-    #name = info.first_name
+    name = info.first_name
     bioo = info.bio
-    
-    anamee = f"<a href='tg://user?id={idd}'>{aname}</a>"
+
     keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(f"{aname}", user_id=idd)
-            ],
-            [
-                InlineKeyboardButton(
-                    "ضيـف البـوت لمجمـوعتـك ✅", url=f"https://t.me/{BOT_USERNAME}?startgroup=new"
-                )
-            ],
-        ]
+        inline_keyboard=[[
+                InlineKeyboardButton(f"{name}", user_id=idd)
+            ]]
     )
 
     # نستخدم async for للحصول على الصور
@@ -43,6 +34,6 @@ async def assistant(c: Client, m: Message):
         # إذا كانت هناك صورة
         await m.reply_photo(
             photos[0].file_id,
-            caption=f"⟡ معلومات الحساب المساعد :\n━━━━━━━━━━━━━\n• 𝙽𝚊𝚖𝚎 ↦ {anamee}\n• 𝚄𝚜𝚎𝚛 ↦ @{usern}\n• 𝙱𝚒𝚘 ↦ {bioo}",
+            caption=f"⟡ معلومات الحساب المساعد :\n━━━━━━━━━━━━━\n• 𝙽𝚊𝚖𝚎 ↦ {aname}\n• 𝚄𝚜𝚎𝚛 ↦ @{usern}\n• 𝙱𝚒𝚘 ↦ {bioo}",
             reply_markup=keyboard
         )
