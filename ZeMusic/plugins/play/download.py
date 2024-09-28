765c279a88121d48674549ab4825fabbcdc61b5a
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-28 22:13:26 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -6,7 +6,7 @@ import yt_dlp
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from youtube_search import YoutubeSearch
-
+from ZeMusic.platforms.Youtube import cookie_txt_file
 from ZeMusic import app
 from ZeMusic.plugins.play.filters import command
 
@@ -52,6 +52,7 @@ async def song_downloader(client, message: Message):
         "geo_bypass": True,
         "outtmpl": f"{title_clean}.%(ext)s",  # استخدام اسم نظيف للملف
         "quiet": True,
+        "cookiefile": cookie_txt_file(),
     }
 
     try:
