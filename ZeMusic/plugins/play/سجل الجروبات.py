65e537bfab25235a74733a801f94557f050b01ca
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-28 06:31:48 +0300

    Update سجل الجروبات.py

--- ZeMusic/plugins/play/سجل الجروبات.py
+++ ZeMusic/plugins/play/سجل الجروبات.py
@@ -1,12 +1,8 @@
 import random
-from pyrogram import Client
-from pyrogram.types import Message
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
-    InputMediaPhoto,
-    InputMediaVideo,
     Message,
 )
 from ZeMusic import app
@@ -21,31 +17,50 @@ photo = [
     "https://te.legra.ph/file/f60ebb75ad6f2786efa4e.jpg",
 ]
 
-async def lul_message(chat_id: int, message: str):
-    await app.send_message(chat_id=chat_id, text=message)
-
-
 @app.on_message(filters.new_chat_members)
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
         added_by = message.from_user.first_name if message.from_user else "مستخدم غير معروف"
-        added_id = message.from_user.id
+        added_id = message.from_user.id if message.from_user else None
 
         matlabi_jhanto = message.chat.title
         served_chats = len(await get_served_chats())
         chat = message.chat
         cont = await app.get_chat_members_count(chat.id)
-        chatusername = (message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜᴘ")
-        lemda_text = f"🌹 تمت اضافه البوت الى مجموعه جديدة .\n\n┏━━━━━━━━━━━━━━━━━┓\n┣★ <b>𝙲𝙷𝙰𝚃</b> › : {matlabi_jhanto}\n┣★ <b>𝙲𝙷𝙰𝚃 𝙸𝙳</b> › : {chat.id}\n┣★ <b>𝙲𝙷𝙰𝚃 𝚄𝙽𝙰𝙼𝙴</b> › : {chatusername}\n┣★ <b>𝙲𝙾𝚄𝙽𝚃</b> › : {cont}\n┣★ <b>𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃</b> › : {served_chats}\n┣★ <b>𝙰𝙳𝙳𝙴𝙳 𝙱𝚈</b> › :\n┗━━━ꪜ <a href='tg://user?id={added_id}'>{added_by}</a>"
+        chatusername = (message.chat.username if message.chat.username else "مجموعة خاصة")
+
+        # إنشاء النص
+        lemda_text = (
+            f"🌹 تمت إضافة البوت إلى مجموعة جديدة.\n\n"
+            f"┏━━━━━━━━━━━━━━━━━┓\n"
+            f"┣★ <b>𝙲𝙷𝙰𝚃</b> › : {matlabi_jhanto}\n"
+            f"┣★ <b>𝙲𝙷𝙰𝚃 𝙸𝙳</b> › : {chat.id}\n"
+            f"┣★ <b>𝙲𝙷𝙰𝚃 𝚄𝙽𝙰𝙼𝙴</b> › : {chatusername}\n"
+            f"┣★ <b>𝙲𝙾𝚄𝙽𝚃</b> › : {cont}\n"
+            f"┣★ <b>𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂</b> › : {served_chats}\n"
+        )
+
+        if added_id:
+            # إذا كان معرف المستخدم متوفر، أضف تفاصيل الشخص الذي أضاف البوت
+            lemda_text += f"┣★ <b>𝙰𝙳𝙳𝙴𝙳 𝙱𝚈</b> › :\n┗━━━ꪜ <a href='tg://user?id={added_id}'>{added_by}</a>"
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            f"{added_by}", user_id=added_id
+                        )
+                    ]
+                ]
+            )
+        else:
+            # إذا كان معرف المستخدم غير متوفر، أضف رسالة عامة
+            lemda_text += "┗━━━ꪜ <b>𝙰𝙳𝙳𝙴𝙳 𝙱𝚈</b> › : مستخدم غير معروف"
+            reply_markup = None
+
+        # إرسال الرسالة مع الصورة
         await app.send_photo(
             LOGGER_ID,
             photo=random.choice(photo),
             caption=lemda_text,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            f"{added_by}", url=f"tg://openmessage?user_id={added_id}")
-                    ]
-                ]
-            ))
+            reply_markup=reply_markup
+        )
