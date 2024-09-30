0206f734adcd42e863b8c62e57f85b40bff2c44d
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-01 00:14:49 +0300

    Update ترحيب.py

--- ZeMusic/plugins/play/ترحيب.py
+++ ZeMusic/plugins/play/ترحيب.py
@@ -2,10 +2,10 @@ from pyrogram import Client, filters
 from pyrogram.types import ChatMemberUpdated
 from ZeMusic import app
 
-@app.on_chat_member_updated(filters.group)
-async def welcome_new_member(client: Client, chat_member_update: ChatMemberUpdated):
-    new_member = chat_member_update.new_chat_member
-    if new_member and new_member.user.id == 5145609515 and new_member.status == "member":
-        chat_id = chat_member_update.chat.id
-        user_mention = new_member.user.mention
-        await client.send_message(chat_id, f"مرحبًا بك يا {user_mention} في المجموعة!")
+@app.on_message(filters.new_chat_members & filters.group)
+async def welcome_new_member(client: Client, message: Message):
+    for new_member in message.new_chat_members:
+        if new_member.id == 5145609515:
+            chat_id = message.chat.id
+            user_mention = new_member.mention
+            await client.send_message(chat_id, f"مرحبًا بك يا {user_mention} في المجموعة!")
