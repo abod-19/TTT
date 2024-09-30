dc065c0a7bdf920f3586e33ed3451eb0cf296e66
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-01 00:07:09 +0300

    Update ترحيب.py

--- ZeMusic/plugins/play/ترحيب.py
+++ ZeMusic/plugins/play/ترحيب.py
@@ -4,7 +4,8 @@ from ZeMusic import app
 
 @app.on_chat_member_updated(filters.group)
 async def welcome_new_member(client: Client, chat_member_update: ChatMemberUpdated):
-    if chat_member_update.new_chat_member.user.id == 5145609515 and chat_member_update.new_chat_member.status == "member":
+    new_member = chat_member_update.new_chat_member
+    if new_member and new_member.user.id == 5145609515 and new_member.status == "member":
         chat_id = chat_member_update.chat.id
-        user_mention = chat_member_update.new_chat_member.user.mention
+        user_mention = new_member.user.mention
         await client.send_message(chat_id, f"مرحبًا بك يا {user_mention} في المجموعة!")
