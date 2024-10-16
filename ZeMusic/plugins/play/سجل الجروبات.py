8dffc52ce0d075f35048baa9fe06450d2439dbee
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-16 16:03:57 +0300

    Update سجل الجروبات.py

--- ZeMusic/plugins/play/سجل الجروبات.py
+++ ZeMusic/plugins/play/سجل الجروبات.py
@@ -1,11 +1,19 @@
 import random
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+    InputMediaVideo,
+    Message,
+)
 from ZeMusic import app
 from ZeMusic.utils.database import get_served_chats
 from config import OWNER_ID, LOGGER_ID
-from pyrogram.enums import ChatMemberStatus
+import asyncio
 from datetime import datetime, timedelta
+from pyrogram.enums import ChatMemberStatus
+
 
 photo_urls = [
     "https://envs.sh/Wi_.jpg",
@@ -19,17 +27,25 @@ photo_urls = [
 async def welcome_new_member(client: Client, message: Message):
     chat = message.chat
     dev_id = OWNER_ID
-    bot_id = (await client.get_me()).id
-
     for new_member in message.new_chat_members:
-        # ترحيب بمطور البوت
         if new_member.id == dev_id:
+            chat_id = message.chat.id
+            #user_mention = new_member.mention
+            #await client.send_message(chat_id, f"مرحبًا بك يا {user_mention} في المجموعة!")
+
             info = await app.get_chat(dev_id)
             name = info.first_name
-            markup = InlineKeyboardMarkup([[InlineKeyboardButton(name, user_id=dev_id)]])
-            
-            photos = [photo async for photo in client.get_chat_photos(dev_id, limit=1)]
-            
+            markup = InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(name, user_id=dev_id)]
+                ]
+            )
+
+            photos = []
+            async for photo in client.get_chat_photos(dev_id, limit=1):
+                
+                photos.append(photo)
+
             if not photos:
                 await message.reply_text(
                     f"↢ مرحباً مطوري <a href='tg://user?id={dev_id}'>{name}</a> نورت الشات ياعزيزي🧸",
@@ -41,59 +57,91 @@ async def welcome_new_member(client: Client, message: Message):
                     caption=f"↢ مرحباً مطوري <a href='tg://user?id={dev_id}'>{name}</a> نورت الشات ياعزيزي🧸",
                     reply_markup=markup
                 )
-        
-        # التعامل مع إضافة البوت إلى مجموعة جديدة
-        elif new_member.id == bot_id:
-            added_by = message.from_user.first_name if message.from_user else "مستخدم غير معروف"
-            added_id = message.from_user.id
-            served_chats = len(await get_served_chats())
-            cont = await app.get_chat_members_count(chat.id)
-            chatusername = message.chat.username or "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜ𝑝"
-            
-            caption = (
-                f"🌹 تمت إضافة البوت إلى مجموعة جديدة.\n\n"
-                f" <b>𝙲𝙷𝙰𝚃</b> › : {chat.title}\n"
-                f" <b>𝙲𝙷𝙰𝚃 𝙸𝙳</b> › : {chat.id}\n"
-                f" <b>𝙲𝙷𝙰𝚃 𝚄𝙽𝙰𝙼𝙴</b> › : @{chatusername}\n"
-                f" <b>𝙲𝙾𝚄𝙽𝚃</b> › : {cont}\n"
-                f" <b>𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂</b> › : {served_chats}\n"
-                f" <b>𝙰𝙳𝙳𝙴𝙳 𝙱𝚈</b> › : <a href='tg://user?id={added_id}'>{added_by}</a>"
+    ############################################################################
+
+    chat = await app.get_chat(message.chat.id)
+    chat_name = chat.title  # اسم الجروب
+    chat_photo = chat.photo  # صورة الجروب
+    
+    chat_id = message.chat.id
+    async for member in client.get_chat_members(chat_id):
+        if member.status == ChatMemberStatus.OWNER:  # جلب منشئ المجموعة فقط
+            owner_id = member.user.id
+            owner_name = member.user.first_name
+     
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[[
+                InlineKeyboardButton(f"{owner_name}", url=f"tg://openmessage?user_id={owner_id}")
+            ]]
+    )       
+    for new_member in message.new_chat_members:
+        first_name = new_member.mention  # اسم العضو الجديد
+        username = new_member.username  # يوزر العضو الجديد
+        # الحصول على الوقت الحالي
+        now = datetime.utcnow()  # استخدام الوقت العالمي UTC
+        # تعديل الوقت ليصبح حسب التوقيت اليمني (UTC +3)
+        yemen_time = now + timedelta(hours=3)
+
+        # تنسيق الوقت والتاريخ
+        join_time = yemen_time.strftime("%I:%M %p")  # الوقت بصيغة 12 ساعة مع AM/PM
+        join_date = yemen_time.strftime("%Y/%m/%d")  # التاريخ بصيغة YYYY/MM/DD
+
+        # إنشاء النص الترحيبي
+        welcome_text = f"""
+𝐰𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩.🧸
+
+__{chat_name}__
+
+➥• Welcome  : {first_name} 
+➥• User : @{username if username else 'No username'}  
+➥• time : {join_time}
+➥• date : {join_date}
+"""
+        # التحقق إذا كانت المجموعة تحتوي على صورة
+        if chat_photo:
+            # تنزيل الصورة وحفظها
+            photo_file = await client.download_media(chat_photo.big_file_id)
+
+            # إرسال الصورة مع النص
+            await message.reply_photo(
+                photo=photo_file,  # استخدام مسار الصورة التي تم تنزيلها
+                caption=welcome_text,
+                reply_markup=keyboard
             )
-            
-            await app.send_photo(
-                LOGGER_ID,
-                photo=random.choice(photo_urls),
-                caption=caption,
-                reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(added_by, url=f"tg://openmessage?user_id={added_id}")]]
-                )
-            )
-        
-        # ترحيب بالأعضاء الجدد
         else:
-            chat_photo = chat.photo
-            async for member in client.get_chat_members(chat.id):
-                if member.status == ChatMemberStatus.OWNER:
-                    owner_id = member.user.id
-                    owner_name = member.user.first_name
-                    break
-            
-            keyboard = InlineKeyboardMarkup(
-                [[InlineKeyboardButton(owner_name, url=f"tg://openmessage?user_id={owner_id}")]]
-            )
+            # إرسال النص فقط إذا لم تكن هناك صورة
+            await message.reply_text(welcome_text, reply_markup=keyboard)
 
-            now = datetime.utcnow() + timedelta(hours=3)
-            welcome_text = (
-                f"𝐰𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩.🧸\n\n"
-                f"__{chat.title}__\n\n"
-                f"➥• Welcome  : {new_member.mention}\n"
-                f"➥• User : @{new_member.username or 'No username'}\n"
-                f"➥• time : {now.strftime('%I:%M %p')}\n"
-                f"➥• date : {now.strftime('%Y/%m/%d')}"
-            )
+    ##########################################################################################
+    # جزء التعامل مع إضافة البوت إلى مجموعة جديدة
+    if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
+        added_by = message.from_user.first_name if message.from_user else "مستخدم غير معروف"
+        added_id = message.from_user.id
 
-            if chat_photo:
-                photo_file = await client.download_media(chat_photo.big_file_id)
-                await message.reply_photo(photo=photo_file, caption=welcome_text, reply_markup=keyboard)
-            else:
-                await message.reply_text(welcome_text, reply_markup=keyboard)
+        matlabi_jhanto = message.chat.title
+        served_chats = len(await get_served_chats())
+        cont = await app.get_chat_members_count(chat.id)
+        chatusername = (message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜ𝑝")
+        lemda_text = (
+            f"🌹 تمت إضافة البوت إلى مجموعة جديدة.\n\n"
+            f"\n"
+            f" <b>𝙲𝙷𝙰𝚃</b> › : {matlabi_jhanto}\n"
+            f" <b>𝙲𝙷𝙰𝚃 𝙸𝙳</b> › : {chat.id}\n"
+            f" <b>𝙲𝙷𝙰𝚃 𝚄𝙽𝙰𝙼𝙴</b> › : @{chatusername}\n"
+            f" <b>𝙲𝙾𝚄𝙽𝚃</b> › : {cont}\n"
+            f" <b>𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂</b> › : {served_chats}\n"
+            f" <b>𝙰𝙳𝙳𝙴𝙳 𝙱𝚈</b> › :\n"
+            f" <a href='tg://user?id={added_id}'>{added_by}</a>"
+        )
+        
+        
+        await app.send_photo(
+            LOGGER_ID,
+            photo=random.choice(photo_urls),
+            caption=lemda_text,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(f"{added_by}", url=f"tg://openmessage?user_id={added_id}")]
+                ]
+            )
+        )
