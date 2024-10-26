5292fa271816a8e859c7867d300f2bf549bc50da
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-27 01:40:22 +0300

    Update انشائي.py

--- ZeMusic/plugins/play/انشائي.py
+++ ZeMusic/plugins/play/انشائي.py
@@ -6,5 +6,5 @@ from ZeMusic import app
 @app.on_message(filters.command(["انشائي"],""))
 async def account_creation(client, message):
     user = await client.get_users(message.from_user.id)
-    creation_date = datetime.datetime.fromtimestamp(user.dc_id)
-    await message.reply(f"تم إنشاء حسابك في: {creation_date.strftime('%Y-%m-%d %H:%M:%S')}")
+    creation_date = datetime.datetime.fromtimestamp(user.date)
+    await message.reply(f"تم إنشاء حسابك في:\n{creation_date.strftime('%Y-%m-%d %H:%M:%S')}")
