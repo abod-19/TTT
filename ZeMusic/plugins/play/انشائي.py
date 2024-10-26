d267531f93cdd50d6d73e519123cc9732c58d957
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-10-27 01:49:46 +0300

    Update انشائي.py

--- ZeMusic/plugins/play/انشائي.py
+++ ZeMusic/plugins/play/انشائي.py
@@ -1,10 +1,13 @@
-from pyrogram import Client, filters
-import datetime
 from ZeMusic import app
+from pyrogram import Client, filters
+from pyrogram.raw.functions.users import GetFullUser
+from pyrogram.raw.types import InputUserSelf
+import datetime
 
 
 @app.on_message(filters.command(["انشائي"],""))
 async def account_creation(client, message):
-    user = await client.get_users(message.from_user.id)
+    user_full_info = await client.invoke(GetFullUser(id=InputUserSelf()))
+    user = user_full_info.users[0]
     creation_date = datetime.datetime.fromtimestamp(user.date)
-    await message.reply(f"تم إنشاء حسابك في:\n{creation_date.strftime('%Y-%m-%d %H:%M:%S')}")
+    await message.reply(f"تم إنشاء حسابك في: {creation_date.strftime('%Y-%m-%d %H:%M:%S')}")
