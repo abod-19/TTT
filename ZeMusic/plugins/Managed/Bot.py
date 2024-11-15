3e110123fb185ef5b48ad0d9a8cde59fc2bcd533
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-11-15 05:56:17 +0300

    Update Bot.py

--- ZeMusic/plugins/Managed/Bot.py
+++ ZeMusic/plugins/Managed/Bot.py
@@ -6,8 +6,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import BOT_NAME
 
 italy = [
-         "قول <pre>{BOT_NAME}</pre> غنيلي",
-         "<pre>هلا</pre>\nلبيه وش اغني لك"
+         "قول <pre style='color: red;'>{BOT_NAME}</pre> غنيلي",  # اللون الأحمر
+         "<pre style='color: blue;'>هلا</pre>\nلبيه وش اغني لك"  # اللون الأزرق
          ]
 
 @app.on_message(filters.regex(r"^(بوت)$"))
@@ -15,4 +15,4 @@ async def Italymusic(client, message):
     if "بوت" in message.text:
         response = random.choice(italy)
         response = response.format(nameuser=message.from_user.first_name, BOT_NAME=BOT_NAME)
-        await message.reply(response)
+        await message.reply(response, parse_mode='html')  # تفعيل التنسيق باستخدام HTML
