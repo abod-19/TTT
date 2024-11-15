a5be2f730b4447c5fd37e7d481e605abe757767c
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-11-15 19:21:20 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -22,10 +22,6 @@ Nem = config.BOT_NAME + " ابحث"
 
 @app.on_message(command(["song", "/song", "بحث", Nem,"يوت"]) & filters.group)
 async def song_downloader(client, message: Message):
-    chat_id = message.chat.id 
-    if not await is_search_enabled(chat_id):
-        return await message.reply_text("<b>⟡عذراً عزيزي اليوتيوب معطل لتفعيل اليوتيوب اكتب تفعيل اليوتيوب</b>")
-        
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث ..</b>")
     
