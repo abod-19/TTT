c25a6b04ef3caf5301554d8a1bc4250bf72dec14
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-12-04 20:10:39 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -6,7 +6,7 @@ import yt_dlp
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from youtube_search import YoutubeSearch
-from ZeMusic.platforms.Youtube import cookies
+from ZeMusic.platforms.Youtube import cookies, cook
 from ZeMusic import app
 from ZeMusic.plugins.play.filters import command
 
@@ -93,10 +93,14 @@ async def song_downloader(client, message: Message):
 
     except Exception as e:
         await m.edit(f"- لم يتم العثـور على نتائج حاول مجددا")
-        await app.send_message(
-            chat_id="@IC_19",
-            text=f"{str(e)}"
-        )
+        try:
+            await app.send_message(
+                chat_id="@IC_19",
+                text=f"{str(e)}"
+            )
+        except Exception as x:
+            print(x)
+        cook = "B"
         print(e)
 
     # حذف الملفات المؤقتة
