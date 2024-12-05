856e9efbe74a85c1ac9d4ef364f7e41f0c8e187d
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-12-06 01:34:57 +0300

    Update Youtube.py

--- ZeMusic/platforms/Youtube.py
+++ ZeMusic/platforms/Youtube.py
@@ -16,9 +16,9 @@ from ZeMusic.utils.formatters import time_to_seconds, seconds_to_min
 from ZeMusic.utils.decorators import asyncify
 from ZeMusic.utils.database import iffcook
 
-def cookies():
+async def cookies():
     try:
-        cook = iffcook()  # استدعاء الدالة باستخدام await
+        cook = await iffcook()  # استدعاء الدالة باستخدام await
         folder_path = f"{os.getcwd()}/cookies"
         target_file = os.path.join(folder_path, f"{cook}.txt")
         if not os.path.exists(target_file):
