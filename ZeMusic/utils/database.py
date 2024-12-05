5749cde767453cf7d3e27c077b3f906245399447
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-12-05 17:26:47 +0300

    Update database.py

--- ZeMusic/utils/database.py
+++ ZeMusic/utils/database.py
@@ -43,16 +43,16 @@ wedb = mongodb.we
 lfdb = mongodb.lf
 iffdb = mongodb.iff
 
-async def iffcook():
+def iffcook():
     settings = await iffdb.find_one({"name": "search"})
     if settings:
         return settings.get("enabled", False)
     return False
 
-async def enable_iff():
+def enable_iff():
     await iffdb.update_one({"name": "search"}, {"$set": {"enabled": True}}, upsert=True)
 
-async def disable_iff():
+def disable_iff():
     await iffdb.update_one({"name": "search"}, {"$set": {"enabled": False}}, upsert=True)
     
 
