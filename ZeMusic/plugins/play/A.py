a50082df57f3f5f74d930982df828f1e175fc811
Abdullah <146959314+abod-19@users.noreply.github.com>
2025-01-07 17:00:49 +0300

    Update A.py

--- ZeMusic/plugins/play/A.py
+++ ZeMusic/plugins/play/A.py
@@ -109,7 +109,7 @@ async def song_downloader(client, message: Message):
         message_to_channel = await app.send_audio(
             chat_id="@IC_l9",  # إرسال الرسالة إلى القناة
             audio=audio_file,
-            caption=f"♥",
+            caption=f"{results[0]["id"]}",
             title=title,
             performer=info_dict.get("uploader", "Unknown"),
             thumb=thumb_name,
