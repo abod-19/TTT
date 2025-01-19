4a6c4f4b89a0344a2a68b21c7f200329e838545f
Abdullah <146959314+abod-19@users.noreply.github.com>
2025-01-19 23:41:01 +0300

    Update Chatgpt.py

--- ZeMusic/plugins/Managed/Chatgpt.py
+++ ZeMusic/plugins/Managed/Chatgpt.py
@@ -5,19 +5,16 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
 
-@app.on_message(filters.command(["chatgpt", "ai", "ask", "", "رون"], prefixes=[".", "J", "j", "s", "", "/"]))
+@app.on_message(filters.command(["رون"],""))
 async def chat_gpt(bot, message):
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
 
-        # Check if name is defined, if not, set a default value
-        name = message.from_user.first_name if message.from_user else "User"
-
         if len(message.command) < 2:
-            await message.reply_text(f"**Hello {name}, How can I help you today?**")
+            await message.reply_text(f"♪  اكتب <p>رون</p> واي شي تريد تسالة راح يجاوبك.")
         else:
             query = message.text.split(' ', 1)[1]
             response = api.gemini(query)["results"]
-            await message.reply_text(f"{response}", parse_mode=ParseMode.MARKDOWN)
+            await message.reply_text(f"<blockquote>{query}</blockquote>\n{response}", parse_mode=ParseMode.MARKDOWN)
     except Exception as e:
-        await message.reply_text(f"**Error: {e}**")
+        await message.reply_text(f"Error: {e}")
