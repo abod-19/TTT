e20cb95cbcbaa1bd20e7c625adf93175b3788f3d
Abdullah <146959314+abod-19@users.noreply.github.com>
2025-01-26 04:51:00 +0300

    Update __init__.py

--- ZeMusic/__init__.py
+++ ZeMusic/__init__.py
@@ -3,14 +3,14 @@ from ZeMusic.core.dir import dirr
 from ZeMusic.core.git import git
 from ZeMusic.core.userbot import Userbot
 from ZeMusic.misc import dbb, heroku
-
+from ZeMusic.core.rd import Coll
 from .logging import LOGGER
 
 dirr()
 git()
 dbb()
 heroku()
-
+rd = Coll()
 app = Mody()
 userbot = Userbot()
 
