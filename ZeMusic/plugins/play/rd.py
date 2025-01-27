e94d731063fee1a57315686800fc51ecd607e002
Abdullah <146959314+abod-19@users.noreply.github.com>
2025-01-27 03:51:47 +0300

    Create rd.py

---
+++ ZeMusic/plugins/play/rd.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+import config
+
+class Rd:
+    def __init__(self):
+        self.one = Client(
+            name="abod",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING1),
+            no_updates=True,
+        )
+
+    async def start(self):
+        # بدء الجلسة
+        await self.one.start()
+        print("الجلسة قيد التشغيل...")
+
+        # إضافة الرد التلقائي
+        @self.one.on_message()
+        async def auto_reply(client, message):
+          print(f"رسالة جديدة من {message.from_user.id}: {message.text}")
+          reply_text = "عندك اي مشكله قم بمراسله المطور @BBFYY"
+          await message.reply_text(reply_text)
+
+        print("الرد التلقائي مفعل.")
+
+    async def stop(self):
+        await self.one.stop()
+        print("تم إيقاف الجلسة.")
