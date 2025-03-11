b563164e8f1fd903e05e75abc461f5f613e9ae3d
Abdullah <146959314+abod-19@users.noreply.github.com>
2025-03-11 03:43:48 +0300

    Update Chatgpt.py

--- ZeMusic/plugins/Managed/Chatgpt.py
+++ ZeMusic/plugins/Managed/Chatgpt.py
@@ -5,16 +5,16 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
 
-@app.on_message(filters.command(["رون"],""))
+@app.on_message(filters.command([""],""))
 async def chat_gpt(bot, message):
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
 
-        if len(message.command) < 2:
-            await message.reply_text(f"♪  اكتب <p>رون</p> واي شي تريد تسالة راح يجاوبك.")
-        else:
-            query = message.text.split(' ', 1)[1]
-            response = api.gemini(query)["results"]
-            await message.reply_text(f"{response}", parse_mode=ParseMode.MARKDOWN)
+        #if len(message.command) < 2:
+            #await message.reply_text(f"♪  اكتب <p>رون</p> واي شي تريد تسالة راح يجاوبك.")
+        #else:
+        query = message.text.split(' ', 1)[1]
+        response = api.gemini(query)["results"]
+        await message.reply_text(f"{response}", parse_mode=ParseMode.MARKDOWN)
     except Exception as e:
         await message.reply_text(f"Error: {e}")
